f8beb4caec5402785767f4f169b9800231435291
jpobrienm <33789137+jpobrienm@users.noreply.github.com>
2021-12-21 13:45:23 -0500

    Se completa una primera version de fileHandler

--- src/test/java/co/com/tallerDDD/Aggregates/FileHandlerTest.java
+++ src/test/java/co/com/tallerDDD/Aggregates/FileHandlerTest.java
@@ -1,4 +1,12 @@
 package co.com.tallerDDD.Aggregates;
 
+import co.com.tallerDDD.DomainEvents.VariableHandlerCreated;
+import org.junit.Test;
+
 public class FileHandlerTest {
+
+    @Test
+    void VariableHandlerCreated(){
+
+    }
 }
