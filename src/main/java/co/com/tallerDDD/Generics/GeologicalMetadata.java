bd2b8db79b34a0e281035bd2cfe09b270b420fc9
jpobrienm <33789137+jpobrienm@users.noreply.github.com>
2021-12-21 23:16:29 -0500

    se añaden validaciones de los objetos de valor que lo necesitan

--- src/main/java/co/com/tallerDDD/Generics/GeologicalMetadata.java
+++ src/main/java/co/com/tallerDDD/Generics/GeologicalMetadata.java
@@ -11,6 +11,12 @@ public class GeologicalMetadata implements Serializable {
 
     public GeologicalMetadata(Integer cellSize, Integer origin, Integer xCoord, Integer yCoord, String geometry){
         geoMetadata = new ArrayList<>();
+        if(cellSize <= 0){
+            throw new IllegalArgumentException("El tamaño de celda no puede ser negativo o cero");
+        }
+        if(xCoord < 0 || yCoord < 0){
+            throw new IllegalArgumentException("Las coordenadas deben ser positivas");
+        }
         geoMetadata.add(cellSize.toString());
         geoMetadata.add(origin.toString());
         geoMetadata.add(xCoord.toString());
